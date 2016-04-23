e5d3df71541a8f20a7b02e20ddc5c118e27aeee5
Niaw de Leon <neehnahw@gmail.com>
2016-04-23 20:21:50 +0800

    First commit

---
+++ source/app/Entry.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entry extends Model
+{
+    protected $table = "hokkiendict";
+    public $timestamps = "false";
+}
---
+++ source/app/Http/routes.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+use App\Task;
+use App\Entry;
+use Illuminate\Http\Request;
+
+
+Route::get('/', function () {
+    $entries = Entry::where('id', '<', '20')->get();
+
+    return view('entries', [
+        'entries' => $entries
+    ]);
+});
+
+/* Add New Task */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+
+});
+
+
+Route::delete('/task/{task}', function (Task $task) {
+	//
+});
+
+
+
