633ebe36fc8fe76da0805340fd2f57a5bc3d2b89
Niaw de Leon <xiaomao@Niaws-MacBook-Pro.local>
2016-05-18 03:11:58 +0800

    add guide

--- source/resources/views/entries.blade.php
+++ source/resources/views/entries.blade.php
@@ -49,6 +49,50 @@
                         @endforeach
                     </tbody>
                 </table>
+				<h2>Pronunciation Guide</h2>
+                <p>The dictionary uses the <a href="http://www.tailingua.com/language/writing/romanizations/peh-oe-ji.php">Pe̍h-ōe-jī (POJ)</a>. Here's a tone guide based on the Philippine variant of Hokkien, also known as Lan-Nang-Oe or Fookien.</p>
+                <table class="table">
+                    <tr>
+                        <th>Tone Number</th>
+                        <th>Tone Mark</th>
+                        <th>Contour</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>a</td>
+                        <td>33</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>á</td>
+                        <td>55</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>à</td>
+                        <td>41</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>ah/ak/ap/at</td>
+                        <td>5</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>â</td>
+                        <td>24</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>ā</td>
+                        <td>41</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>a̍</td>
+                        <td>24</td>
+                    </tr>
+                </table>
             </div>
         </div>
         @endif
