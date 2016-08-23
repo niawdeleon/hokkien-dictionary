3bbd2c881087df4fb1f8cc252c4958def20e758d
Niaw de Leon <niawdeleon@users.noreply.github.com>
2016-08-23 23:10:22 +0800

    changed title

--- source/resources/views/layouts/app.blade.php
+++ source/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>English-Hokkien Dictionary</title>
+        <title>English-Fookien Dictionary</title>
 
         <!-- CSS And JavaScript -->
         <!-- Bootstrap -->
